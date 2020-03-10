340ed3df56131c54356127516e0aa4e26fc9fc03
RB704 <42446410+RB704@users.noreply.github.com>
2020-03-11 00:51:17 +0530

    Add files via upload

---
+++ database_Access_Layer/db.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace MVCAPI.database_Access_Layer
+{
+    public class db
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["con"].ConnectionString);
+
+        public DataSet GetRecordbyid(int id)
+        {
+            SqlCommand com = new SqlCommand("SpgetDetailbyid", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@Sr_no", id);
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+        }
+    }
+}
